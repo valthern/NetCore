80e8cb738372f57b68cfd281aeb207ecedc66dda
Omar I. Martínez Gómez <omar_238@hotmail.com>
2022-01-26 22:53:13 -0600

    efcore -> 2.19 Empezando cambiando nombres y esquema

---
+++ EFCorePeliculas/EFCorePeliculas/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using EFCorePeliculas.Entidades;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCorePeliculas
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Genero> Generos { get; set; }
+
+        public ApplicationDbContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Genero>().HasKey(prop => prop.Identificador);
+            modelBuilder.Entity<Genero>().Property(prop => prop.Nombre).HasMaxLength(150).IsRequired();
+        }
+    }
+}
---
+++ EFCorePeliculas/EFCorePeliculas/Entidades/Genero.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace EFCorePeliculas.Entidades
+{
+    public class Genero
+    {
+        //[Key]
+        public int Identificador { get; set; }
+        //[StringLength(150)]
+        //[MaxLength(150)]
+        //[Required]
+        public string Nombre { get; set; }
+    }
+}
