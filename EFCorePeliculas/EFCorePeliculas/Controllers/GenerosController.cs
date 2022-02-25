6de3960b6ac2dbc3c4f3d25e12bc3de0fa383836
Omar I. Martínez Gómez <omar_238@hotmail.com>
2022-02-24 23:02:36 -0600

    efcore -> 3.36 terminando el video de un simple query

--- EFCorePeliculas/EFCorePeliculas/Controllers/GenerosController.cs
+++ EFCorePeliculas/EFCorePeliculas/Controllers/GenerosController.cs
@@ -1,6 +1,24 @@
-﻿namespace EFCorePeliculas.Controllers
+﻿using EFCorePeliculas.Entidades;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCorePeliculas.Controllers
 {
-    public class GenerosController
+    [ApiController]
+    [Route("api/generos")]
+    public class GenerosController:ControllerBase
     {
+        private readonly ApplicationDbContext context;
+
+        public GenerosController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Genero>> Get()
+        {
+            return await context.Generos.ToListAsync();
+        }
     }
 }
