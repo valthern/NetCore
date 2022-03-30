a001a0ad769f05dbb2bcf49ec5fd7a4123a764f6
Omar I. Martínez Gómez <omar_238@hotmail.com>
2022-03-30 15:58:38 -0600

    efcore -> 3.44 Consultando Daos Espaciales, creando CinesController

---
+++ EFCorePeliculas/EFCorePeliculas/Controllers/CinesController.cs
@@ -0,0 +1,24 @@
+﻿using EFCorePeliculas.Entidades;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCorePeliculas.Controllers
+{
+    [ApiController]
+    [Route("api/cines")]
+    public class CinesController:ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+
+        public CinesController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Cine>> Get()
+        {
+            return await context.Cines.ToListAsync();
+        }
+    }
+}
