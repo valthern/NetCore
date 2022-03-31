5fb88a28c9b3de10efd1b22818ca2f2f2030bdf8
Omar I. Martínez Gómez <omar_238@hotmail.com>
2022-03-30 22:22:39 -0600

    efcore -> 3.46 Empezando Eager loading - include

--- EFCorePeliculas/EFCorePeliculas/Controllers/CinesController.cs
+++ EFCorePeliculas/EFCorePeliculas/Controllers/CinesController.cs
@@ -1,6 +1,10 @@
-﻿using EFCorePeliculas.Entidades;
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using EFCorePeliculas.DTOs;
+using EFCorePeliculas.Entidades;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using NetTopologySuite;
 
 namespace EFCorePeliculas.Controllers
 {
@@ -9,16 +13,38 @@ namespace EFCorePeliculas.Controllers
     public class CinesController:ControllerBase
     {
         private readonly ApplicationDbContext context;
+        private readonly IMapper mapper;
 
-        public CinesController(ApplicationDbContext context)
+        public CinesController(ApplicationDbContext context, IMapper mapper)
         {
             this.context = context;
+            this.mapper = mapper;
         }
 
         [HttpGet]
-        public async Task<IEnumerable<Cine>> Get()
+        public async Task<IEnumerable<CineDTO>> Get()
         {
-            return await context.Cines.ToListAsync();
+            return await context.Cines.ProjectTo<CineDTO>(mapper.ConfigurationProvider).ToListAsync();
+        }
+
+        [HttpGet("cercanos")]
+        public async Task<ActionResult> Get(double latitud, double longitud)
+        {
+            var geometryFactory = NtsGeometryServices.Instance.CreateGeometryFactory(srid: 4326);
+
+            var miUbicacion = geometryFactory.CreatePoint(new NetTopologySuite.Geometries.Coordinate(longitud, latitud));
+            var distanciaMáximaEnMetros = 2000;
+
+            var cines=await context.Cines
+                .OrderBy(c=>c.Ubicacion.Distance(miUbicacion))
+                .Where(c=>c.Ubicacion.IsWithinDistance(miUbicacion,distanciaMáximaEnMetros))
+                .Select(c=>new
+                {
+                    Nombre=c.Nombre,
+                    Distancia=Math.Round(c.Ubicacion.Distance(miUbicacion))
+                }).ToListAsync();
+
+            return Ok(cines);
         }
     }
 }
--- EFCorePeliculas/EFCorePeliculas/Servicios/AutoMapperProfiles.cs
+++ EFCorePeliculas/EFCorePeliculas/Servicios/AutoMapperProfiles.cs
@@ -4,11 +4,15 @@ using EFCorePeliculas.Entidades;
 
 namespace EFCorePeliculas.Servicios
 {
-    public class AutoMapperProfiles:Profile
+    public class AutoMapperProfiles : Profile
     {
         public AutoMapperProfiles()
         {
             CreateMap<Actor, ActorDTO>();
+
+            CreateMap<Cine, CineDTO>()
+                .ForMember(dto => dto.Latitud, ent => ent.MapFrom(prop => prop.Ubicacion.Y))
+                .ForMember(dto => dto.longitud, ent => ent.MapFrom(prop => prop.Ubicacion.X));
         }
     }
 }
