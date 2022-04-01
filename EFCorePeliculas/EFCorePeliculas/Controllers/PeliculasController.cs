bc12c34a21a7d4023e797b1cc730c7173367f9c5
Omar I. Martínez Gómez <omar_238@hotmail.com>
2022-03-31 18:02:16 -0600

    efcore -> 3.47 empezando Utilizando ThenInclude

---
+++ EFCorePeliculas/EFCorePeliculas/Controllers/PeliculasController.cs
@@ -0,0 +1,29 @@
+﻿using EFCorePeliculas.Entidades;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCorePeliculas.Controllers
+{
+    public class PeliculasController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+
+        public PeliculasController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Pelicula>> Get(int id)
+        {
+            var pelicula = await context.Peliculas
+                .Include(p => p.Generos)
+                .FirstOrDefaultAsync(p => p.Id == id);
+
+            if (pelicula is null)
+                return NotFound();
+
+            return pelicula;
+        }
+    }
+}
